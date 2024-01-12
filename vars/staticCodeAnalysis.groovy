14c132b450e8d9e71fd590ca1c5ea95a4c65ec7d
mateenshaik <mateenshaikh046@gmail.com>
2024-01-12 10:32:12 +0530

    var filea add

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,4 +1,4 @@
-def call(){
-    withSonarQubeEnv(credentialsId: 'sonar-token')
+def call(credentialsId){
+    withSonarQubeEnv(credentialsId: credentialsId)
     sh 'mvn clean package sonar:sonar'
 }
