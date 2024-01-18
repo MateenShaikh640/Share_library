5b7a6b090c7c1ebf3c8be1b66977f0f4810ef8ab
mateenshaik <mateenshaikh046@gmail.com>
2024-01-18 12:45:50 +0530

    i made change in jenkins file

--- vars/dockerimageCleanup.groovy
+++ vars/dockerimageCleanup.groovy
@@ -1,4 +1,11 @@
+// sh """
+//    sh docker rmi ${hubUser}/${project}:${ImageTag}
+//     sh docker rmi ${hubUser}/${project}:latest
+//    """ 
+
+ def call(String aws_account_id, String region, String reponame){
 sh """
-   sh docker rmi ${hubUser}/${project}:${ImageTag}
-    sh docker rmi ${hubUser}/${project}:latest
-   """ 
+   docker rmi ${reponame}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${reponame}:latest
+
+"""
+ }
