1ead49ac620b8afafc8ac190822226656a602d05
mateenshaik <mateenshaikh046@gmail.com>
2024-01-18 12:48:51 +0530

    i made change in jenkins file

--- vars/dockerimagePush.groovy
+++ vars/dockerimagePush.groovy
@@ -13,7 +13,7 @@
  def call(String aws_account_id, String region, String reponame){
 sh """
    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-   docker push ${reponame}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${reponame}:latest
+   docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${reponame}:latest
 
 """
  }
