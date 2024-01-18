9e8d2f3dad7750c85091051c274587f9ecf564b9
mateenshaik <mateenshaikh046@gmail.com>
2024-01-18 14:52:35 +0530

    i made change in jenkins file

--- vars/dockerimagePush.groovy
+++ vars/dockerimagePush.groovy
@@ -12,10 +12,9 @@
 
  def call(String aws_account_id, String region, String reponame){
 sh """
-       aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 316958124144.dkr.ecr.ap-south-1.amazonaws.com
-       docker push 316958124144.dkr.ecr.ap-south-1.amazonaws.com/mateenshaikh01:latest
+    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+   docker push${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${reponame}:latest
 """
-//    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-//    docker push${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${reponame}:latest
-
+   //docker push 316958124144.dkr.ecr.ap-south-1.amazonaws.com/mateenshaikh01:latest
+  //aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 316958124144.dkr.ecr.ap-south-1.amazonaws.com
  }
